2cda2db387d563eda8e32cb2da6b041b4cad3d76
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2024-03-07 22:31:36 +0900

    Set keyframes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,43 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        overlayshow: {
+          from: {
+            opacity: 0,
+          },
+          to: {
+            opacity: 1,
+          },
+        },
+        overlayhide: {
+          from: {
+            opacity: 1,
+          },
+          to: {
+            opacity: 0,
+          },
+        },
+        contentshow: {
+          from: {
+            transform: "translate(-50%, 100%)",
+          },
+          to: {
+            transform: "translate(-50%, 0)",
+          },
+        },
+        contenthide: {
+          from: {
+            transform: "translate(-50%, 0)",
+          },
+          to: {
+            transform: "translate(-50%, 110%)",
+          },
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
