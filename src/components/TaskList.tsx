cdbd30881c4823471781ed64823de6005e6274a3
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2024-03-09 21:17:46 +0900

    Create TaskList component

---
+++ src/components/TaskList.tsx
@@ -0,0 +1,383 @@
+import { v4 as uuid } from "uuid";
+import { useState, useRef } from "react";
+import {
+  DndContext,
+  closestCenter,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+  DragEndEvent,
+  DragStartEvent,
+  DragCancelEvent,
+} from "@dnd-kit/core";
+import {
+  useSortable,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  verticalListSortingStrategy,
+  arrayMove,
+} from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+import { restrictToVerticalAxis } from "@dnd-kit/modifiers";
+import {
+  Root as Checkbox,
+  Indicator as CheckboxIndicator,
+} from "@radix-ui/react-checkbox";
+import { CheckIcon } from "@radix-ui/react-icons";
+import { clsx } from "clsx";
+
+import { Icon } from "components/Icon";
+import { Sheet } from "components/Sheet";
+import { Textarea } from "components/Textarea";
+
+function InvitationSheet(props: {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}) {
+  return (
+    <Sheet open={props.open} onOpenChange={props.onOpenChange} title="招待">
+      <div>Invite!</div>
+    </Sheet>
+  );
+}
+
+export function TaskList(props: {
+  taskList: TaskList;
+  handleTaskChange: (updatedTask: Task) => void;
+  handleTaskListChange: (updateTaskList: TaskList) => void;
+  handleDragStart: (e: DragStartEvent) => void;
+  handleDragCancel: (e: DragCancelEvent) => void;
+  handleDragEnd: (e: DragEndEvent) => void;
+}) {
+  const [taskText, setTaskText] = useState<string>("");
+  const [insertPosition, setInsertPosition] = useState<"bottom" | "top">(
+    "bottom"
+  );
+  const [isShiftPressed, setIsShiftPressed] = useState<boolean>(false);
+  const [invitationSheetOpen, setInvitationSheetOpen] = useState(false);
+
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  );
+
+  const taskList = props.taskList;
+  const tasks = taskList.tasks;
+  const isInsertTop =
+    (insertPosition === "top" && !isShiftPressed) ||
+    (insertPosition === "bottom" && isShiftPressed);
+
+  return (
+    <>
+      <div className="h-full overflow-scroll">
+        <header className="sticky w-full top-0 border-b z-20 bg-white">
+          <section className="px-2">
+            <div className="relative">
+              <h1 className="py-2 text-center font-bold">{taskList.name}</h1>
+              <button
+                className="absolute top-0 right-0 h-full flex items-center justify-center px-2"
+                onClick={() => {
+                  setInvitationSheetOpen(true);
+                }}
+              >
+                <Icon text="groups" />
+              </button>
+            </div>
+            <form
+              className="flex items-center py-2 bg-white"
+              onSubmit={(e) => {
+                e.preventDefault();
+                if (taskText === "") {
+                  return;
+                }
+                const newTask = {
+                  id: uuid(),
+                  text: taskText,
+                  complete: false,
+                  date: "",
+                };
+                if (isInsertTop) {
+                  props.handleTaskListChange({
+                    ...props.taskList,
+                    tasks: [newTask, ...tasks],
+                  });
+                } else {
+                  props.handleTaskListChange({
+                    ...props.taskList,
+                    tasks: [...tasks, newTask],
+                  });
+                }
+                setTaskText("");
+              }}
+            >
+              <span
+                className="p-2 flex"
+                onClick={() => {
+                  setInsertPosition(
+                    insertPosition === "bottom" ? "top" : "bottom"
+                  );
+                }}
+              >
+                {isInsertTop ? (
+                  <Icon text="vertical_align_top" />
+                ) : (
+                  <Icon text="vertical_align_bottom" />
+                )}
+              </span>
+              <Textarea
+                className="flex-1 rounded-full py-2 px-4 border"
+                value={taskText}
+                placeholder={
+                  isInsertTop ? "Add task to top" : "Add task to bottom"
+                }
+                onChange={(e) => {
+                  setTaskText(e.currentTarget.value);
+                }}
+                onKeyDown={(e) => setIsShiftPressed(e.shiftKey)}
+                onKeyUp={(e) => setIsShiftPressed(e.shiftKey)}
+              />
+              <button className="p-2 flex" type="submit">
+                <Icon text="send" />
+              </button>
+            </form>
+          </section>
+          <section className="flex py-2 pl-4 pr-3 text-gray-400">
+            <button
+              className="flex"
+              onClick={() => {
+                const newTaskList = {
+                  ...taskList,
+                  tasks: taskList.tasks
+                    .sort((a, b) => {
+                      if (a.complete && !b.complete) {
+                        return 1;
+                      }
+                      if (!a.complete && b.complete) {
+                        return -1;
+                      }
+                      if (!a.date && b.date) {
+                        return 1;
+                      }
+                      if (a.date && !b.date) {
+                        return -1;
+                      }
+                      if (a.date > b.date) {
+                        return 1;
+                      }
+                      if (a.date < b.date) {
+                        return -1;
+                      }
+                      return 0;
+                    })
+                    .concat(),
+                };
+                props.handleTaskListChange(newTaskList);
+              }}
+            >
+              <Icon text="sort" />
+              <span className="pl-1">Sort</span>
+            </button>
+            <div className="inline-block flex-1"></div>
+            <button
+              className="flex"
+              onClick={() => {
+                const newTaskList = {
+                  ...taskList,
+                  tasks: taskList.tasks.filter((t) => !t.complete),
+                };
+                props.handleTaskListChange(newTaskList);
+              }}
+            >
+              <span className="pr-1">Clear Completed</span>
+              <Icon text="delete" />
+            </button>
+          </section>
+        </header>
+
+        <section>
+          <DndContext
+            sensors={sensors}
+            collisionDetection={closestCenter}
+            modifiers={[restrictToVerticalAxis]}
+            onDragStart={props.handleDragStart}
+            onDragCancel={props.handleDragCancel}
+            onDragEnd={(event: DragEndEvent) => {
+              props.handleDragEnd(event);
+              const { active, over } = event;
+
+              if (active && over && active.id !== over.id) {
+                const oldIndex = tasks.findIndex((t) => t.id === active.id);
+                const newIndex = tasks.findIndex((t) => t.id === over.id);
+
+                const newTasks = arrayMove(tasks, oldIndex, newIndex);
+                props.handleTaskListChange({
+                  ...taskList,
+                  tasks: newTasks,
+                });
+              }
+            }}
+          >
+            <SortableContext
+              items={tasks}
+              strategy={verticalListSortingStrategy}
+            >
+              {tasks.map((task, i) => (
+                <TaskItem
+                  key={task.id}
+                  index={i}
+                  task={task}
+                  newTaskText={taskText}
+                  handleTaskChange={(newTask) => {
+                    props.handleTaskChange(newTask);
+                  }}
+                  handleInsertTaskButtonClick={(idx) => {
+                    const newTasks = [...taskList.tasks];
+                    newTasks.splice(idx, 0, {
+                      id: uuid(),
+                      text: taskText,
+                      complete: false,
+                      date: "",
+                    });
+                    props.handleTaskListChange({
+                      ...taskList,
+                      tasks: newTasks,
+                    });
+                    setTaskText("");
+                  }}
+                />
+              ))}
+            </SortableContext>
+          </DndContext>
+        </section>
+      </div>
+
+      <InvitationSheet
+        open={invitationSheetOpen}
+        onOpenChange={setInvitationSheetOpen}
+      />
+    </>
+  );
+}
+
+function TaskItem(props: {
+  index: number;
+  task: Task;
+  newTaskText: string;
+  handleTaskChange: (task: Task) => void;
+  handleInsertTaskButtonClick: (idx: number) => void;
+}) {
+  const task = props.task;
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    setActivatorNodeRef,
+    transform,
+    transition,
+    isDragging,
+    isSorting,
+  } = useSortable({ id: task.id });
+  const dateInputRef = useRef<HTMLInputElement>(null);
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
+  const mustHaveClassNames = ["touch-none"];
+
+  return (
+    <div
+      ref={setNodeRef}
+      style={style}
+      className={clsx(
+        "relative flex border-b",
+        isDragging && "z-10 border-t",
+        task.complete ? "bg-gray-100" : "bg-white"
+      )}
+    >
+      {props.newTaskText && props.index === 0 && !isSorting ? (
+        <button
+          className="flex items-center justify-center absolute z-10 top-0 right-12 translate-y-[-50%] bg-white rounded-full w-8 h-8 border text-gray-400"
+          onClick={() => props.handleInsertTaskButtonClick(0)}
+        >
+          <Icon text="add" />
+        </button>
+      ) : null}
+      <span
+        ref={setActivatorNodeRef}
+        {...attributes}
+        {...listeners}
+        className={clsx(
+          "flex items-center justify-center py-2 pl-3 pr-2 text-gray-400",
+          mustHaveClassNames
+        )}
+      >
+        <Icon text="drag_indicator" />
+      </span>
+      <span className="flex items-center pr-2 py-2">
+        <Checkbox
+          className="border flex w-6 h-6 justify-center items-center rounded-full overflow-hidden"
+          checked={task.complete}
+          onCheckedChange={(v: boolean) => {
+            props.handleTaskChange({
+              ...task,
+              complete: v,
+            });
+          }}
+        >
+          <CheckboxIndicator className="bg-gray-100 text-gray-400 w-full h-full justify-center items-center flex">
+            <CheckIcon />
+          </CheckboxIndicator>
+        </Checkbox>
+      </span>
+      <input
+        type="text"
+        className={clsx(
+          "flex-1 py-4 bg-transparent",
+          task.complete ? "line-through text-gray-400" : ""
+        )}
+        value={task.text}
+        onChange={(e) => {
+          props.handleTaskChange({
+            ...task,
+            text: e.currentTarget.value,
+          });
+        }}
+      />
+      <label
+        className="flex items-center justify-center pl-2 pr-4 py-2 text-gray-400 cursor-pointer"
+        onClick={() => {
+          dateInputRef.current?.focus();
+          dateInputRef.current?.showPicker();
+        }}
+      >
+        <input
+          ref={dateInputRef}
+          className="w-0"
+          type="date"
+          value={task.date}
+          onInput={(e) => {
+            /* FYI: On iOS, default RESET value is the day. For to be empty, set defaultValue blank. And it only works in onInput instead of onChange */
+            e.currentTarget.defaultValue = "";
+            props.handleTaskChange({
+              ...task,
+              date: e.currentTarget.value,
+            });
+          }}
+        />
+        {task.date || <Icon text="event" />}
+      </label>
+      {props.newTaskText && !isSorting ? (
+        <button
+          className="flex items-center justify-center absolute z-10 bottom-0 right-12 translate-y-[50%] bg-white rounded-full w-8 h-8 border text-gray-400"
+          onClick={() => props.handleInsertTaskButtonClick(props.index + 1)}
+        >
+          <Icon text="add" />
+        </button>
+      ) : null}
+    </div>
+  );
+}
