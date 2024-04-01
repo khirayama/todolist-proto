ec5e0bfbe3907fb18f651900f7eeb528b5cca156
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2024-04-01 22:08:45 +0900

    Update useGlobalState

--- src/libs/index.tsx
+++ src/libs/index.tsx
@@ -48,4 +48,83 @@ export const GlobalStateProvider = (props: { children: ReactNode }) => {
   );
 };
 
-export const useGlobalState = () => useContext(GlobalStateContext);
+export const useGlobalState = (): [
+  State,
+  {
+    updatePreferences: (newPreferences: Partial<Preferences>) => void;
+    updateTaskList: (newTaskList: TaskList) => void;
+    updateTaskLists: (newTaskLists: TaskList[]) => void;
+    updateTask: (taskList: TaskList, newTask: Task) => void;
+  },
+] => {
+  const [globalState, setGlobalState] = useContext(GlobalStateContext);
+
+  const updatePreferences = (newPreferences: Partial<Preferences>) => {
+    setGlobalState({
+      ...globalState,
+      app: {
+        ...globalState.app,
+        preferences: {
+          ...globalState.app.preferences,
+          ...newPreferences,
+        },
+      },
+    });
+  };
+
+  const updateTaskList = (newTaskList: TaskList) => {
+    setGlobalState({
+      ...globalState,
+      taskLists: {
+        ...globalState.taskLists,
+        [newTaskList.id]: newTaskList,
+      },
+    });
+  };
+
+  const updateTaskLists = (newTaskLists: TaskList[]) => {
+    const newTaskListsMap = {
+      ...globalState.taskLists,
+    };
+    newTaskLists.forEach((tl) => {
+      newTaskListsMap[tl.id] = tl;
+    });
+
+    setGlobalState({
+      ...globalState,
+      app: {
+        ...globalState.app,
+        taskListIds: newTaskLists.map((tl) => tl.id),
+      },
+      taskLists: newTaskListsMap,
+    });
+  };
+
+  const updateTask = (taskList: TaskList, newTask: Task) => {
+    setGlobalState({
+      ...globalState,
+      taskLists: {
+        ...globalState.taskLists,
+        [taskList.id]: {
+          ...taskList,
+          tasks: taskList.tasks.map((t) => {
+            if (t.id === newTask.id) {
+              return newTask;
+            }
+            return t;
+          }),
+        },
+      },
+    });
+  };
+
+  return [
+    globalState,
+    {
+      updatePreferences,
+      updateTaskList,
+      updateTaskLists,
+      updateTask,
+    },
+  ];
+};
