07bac16aa956a58a22b901292bad26e12de6e3f0
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2024-04-19 22:33:52 +0900

    Add props for components related DatePicker

--- src/components/DatePickerSheet.tsx
+++ src/components/DatePickerSheet.tsx
@@ -4,10 +4,11 @@ import { useTranslation } from "react-i18next";
 import { DatePicker } from "libs/components/DatePicker";
 
 export function DatePickerSheet(props: {
-  task: Task;
+  value: string;
   open: boolean;
   onOpenChange: (open: boolean) => void;
   handleChange: (val: string) => void;
+  handleCancel: () => void;
 }) {
   const { t } = useTranslation();
   const tr = (key: string) => t(`components.DatePickerSheet.${key}`);
@@ -21,8 +22,9 @@ export function DatePickerSheet(props: {
       <div className="px-12">
         <DatePicker
           autoFocus
-          value={props.task.date}
+          value={props.value}
           handleChange={props.handleChange}
+          handleCancel={props.handleCancel}
         />
       </div>
     </Sheet>
