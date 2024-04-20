c7b132ee61a21f0457b01fe429ae1c71e3a5a397
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2024-04-21 08:54:19 +0900

    Use DatePickerSheet in TaskItem

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -1,4 +1,4 @@
-import { useRef, FormEvent, KeyboardEvent } from "react";
+import { FormEvent, KeyboardEvent, useState } from "react";
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
 import {
@@ -9,6 +9,7 @@ import { CheckIcon } from "@radix-ui/react-icons";
 import { clsx } from "clsx";
 
 import { Icon } from "libs/components/Icon";
+import { DatePickerSheet } from "components/DatePickerSheet";
 
 function TaskTextArea(props: {
   task: Task;
@@ -47,7 +48,10 @@ export function TaskItem(props: {
   newTaskText: string;
   handleTaskChange: (task: Task) => void;
   handleInsertTaskButtonClick: (idx: number) => void;
-  handleTaskListItemKeyDown: (e: KeyboardEvent, task: Task) => void;
+  handleTaskListItemKeyDown: (
+    e: KeyboardEvent,
+    options: { task: Task; setDatePickerSheetOpen: (open: boolean) => void }
+  ) => void;
 }) {
   const task = props.task;
   const {
@@ -60,103 +64,115 @@ export function TaskItem(props: {
     isDragging,
     isSorting,
   } = useSortable({ id: task.id });
-  const dateInputRef = useRef<HTMLInputElement>(null);
 
   const style = {
     transform: CSS.Transform.toString(transform),
     transition,
   };
   const mustHaveClassNames = ["touch-none"];
+  const [datePickerSheetOpen, setDatePickerSheetOpen] = useState(false);
 
   return (
-    <div
-      data-taskid={task.id}
-      ref={setNodeRef}
-      style={style}
-      className={clsx(
-        "relative flex border-b",
-        isDragging && "z-10 border-t",
-        task.complete ? "bg-gray-100" : "bg-white"
-      )}
-    >
-      {props.newTaskText && props.index === 0 && !isSorting ? (
-        <button
-          className="flex items-center justify-center absolute z-10 top-0 right-12 translate-y-[-50%] bg-white rounded-full w-8 h-8 border text-gray-400"
-          onClick={() => props.handleInsertTaskButtonClick(0)}
-        >
-          <Icon text="add" />
-        </button>
-      ) : null}
-      <span
-        ref={setActivatorNodeRef}
-        {...attributes}
-        {...listeners}
+    <>
+      <div
+        data-taskid={task.id}
+        ref={setNodeRef}
+        style={style}
         className={clsx(
-          "flex items-center justify-center py-2 pl-3 pr-2 text-gray-400",
-          mustHaveClassNames
+          "relative flex border-b",
+          isDragging && "z-10 border-t",
+          task.complete ? "bg-gray-100" : "bg-white"
         )}
       >
-        <Icon text="drag_indicator" />
-      </span>
-      <span className="flex items-center pr-4 py-2">
-        <Checkbox
-          className="border flex w-6 h-6 justify-center items-center rounded-full overflow-hidden"
-          checked={task.complete}
-          onCheckedChange={(v: boolean) => {
+        {props.newTaskText && props.index === 0 && !isSorting ? (
+          <button
+            className="flex items-center justify-center absolute z-10 top-0 right-12 translate-y-[-50%] bg-white rounded-full w-8 h-8 border text-gray-400"
+            onClick={() => props.handleInsertTaskButtonClick(0)}
+          >
+            <Icon text="add" />
+          </button>
+        ) : null}
+        <span
+          ref={setActivatorNodeRef}
+          {...attributes}
+          {...listeners}
+          className={clsx(
+            "flex items-center justify-center py-2 pl-3 pr-2 text-gray-400",
+            mustHaveClassNames
+          )}
+        >
+          <Icon text="drag_indicator" />
+        </span>
+        <span className="flex items-center pr-4 py-2">
+          <Checkbox
+            className="border flex w-6 h-6 justify-center items-center rounded-full overflow-hidden"
+            checked={task.complete}
+            onCheckedChange={(v: boolean) => {
+              props.handleTaskChange({
+                ...task,
+                complete: v,
+              });
+            }}
+          >
+            <CheckboxIndicator className="bg-gray-100 text-gray-400 w-full h-full justify-center items-center flex">
+              <CheckIcon />
+            </CheckboxIndicator>
+          </Checkbox>
+        </span>
+        <TaskTextArea
+          task={task}
+          // onFocus, onBlurで、focus状態か保存して、DatePickerから戻ってきた時に維持するか考える
+          onTaskTextChange={(e) => {
             props.handleTaskChange({
               ...task,
-              complete: v,
+              text: e.currentTarget.value,
             });
           }}
-        >
-          <CheckboxIndicator className="bg-gray-100 text-gray-400 w-full h-full justify-center items-center flex">
-            <CheckIcon />
-          </CheckboxIndicator>
-        </Checkbox>
-      </span>
-      <TaskTextArea
-        task={task}
-        onTaskTextChange={(e) => {
-          props.handleTaskChange({
-            ...task,
-            text: e.currentTarget.value,
-          });
-        }}
-        onTaskTextKeyDown={(e) => {
-          props.handleTaskListItemKeyDown(e, task);
-        }}
-      />
-      <label
-        className="flex items-center justify-center pl-2 pr-4 py-2 text-gray-400 cursor-pointer"
-        onClick={() => {
-          dateInputRef.current?.focus();
-          dateInputRef.current?.showPicker();
-        }}
-      >
-        <input
-          ref={dateInputRef}
-          className="w-0"
-          type="date"
-          value={task.date}
-          onInput={(e) => {
-            /* FYI: On iOS, default RESET value is the day. For to be empty, set defaultValue blank. And it only works in onInput instead of onChange */
-            e.currentTarget.defaultValue = "";
-            props.handleTaskChange({
-              ...task,
-              date: e.currentTarget.value,
+          onTaskTextKeyDown={(e) => {
+            props.handleTaskListItemKeyDown(e, {
+              task,
+              setDatePickerSheetOpen,
             });
           }}
         />
-        {task.date || <Icon text="event" />}
-      </label>
-      {props.newTaskText && !isSorting ? (
-        <button
-          className="flex items-center justify-center absolute z-10 bottom-0 right-12 translate-y-[50%] bg-white rounded-full w-8 h-8 border text-gray-400"
-          onClick={() => props.handleInsertTaskButtonClick(props.index + 1)}
+        <label
+          className="flex items-center justify-center pl-2 pr-4 py-2 text-gray-400 cursor-pointer"
+          onClick={() => {
+            setDatePickerSheetOpen(true);
+          }}
         >
-          <Icon text="add" />
-        </button>
-      ) : null}
-    </div>
+          {task.date || <Icon text="event" />}
+        </label>
+        {props.newTaskText && !isSorting ? (
+          <button
+            className="flex items-center justify-center absolute z-10 bottom-0 right-12 translate-y-[50%] bg-white rounded-full w-8 h-8 border text-gray-400"
+            onClick={() => props.handleInsertTaskButtonClick(props.index + 1)}
+          >
+            <Icon text="add" />
+          </button>
+        ) : null}
+      </div>
+
+      <DatePickerSheet
+        value={task.date}
+        open={datePickerSheetOpen}
+        onOpenChange={setDatePickerSheetOpen}
+        handleChange={(v) => {
+          props.handleTaskChange({
+            ...task,
+            date: v,
+          });
+          setDatePickerSheetOpen(false);
+          /* TODO: すでにテキストにフォーカスしてる場合のみ、Sheetクローズ時にフォーカスを戻す */
+          const t = document.querySelector<HTMLTextAreaElement>(
+            `[data-taskid="${task.id}"] textarea`
+          );
+          t.selectionStart = t.value.length;
+          t.selectionEnd = t.value.length;
+          t?.focus();
+        }}
+        handleCancel={() => setDatePickerSheetOpen(false)}
+      />
+    </>
   );
 }
