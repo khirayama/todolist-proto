9cc2e9f0b664c4a073d7b2cafbfbd07b02797dc1
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2024-04-20 13:10:24 +0900

    Update DatePicker component

--- src/libs/components/DatePicker.tsx
+++ src/libs/components/DatePicker.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import {
   addDays,
@@ -9,18 +9,28 @@ import {
   endOfWeek,
   format,
   startOfMonth,
+  isBefore,
+  isAfter,
 } from "date-fns";
 import clsx from "clsx";
 
-export function DatePicker() {
+export function DatePicker(props: {
+  value: string;
+  autoFocus?: boolean;
+  handleChange: (val: string) => void;
+  handleCancel: () => void;
+}) {
   const { t } = useTranslation();
-  const tr = (key: string) => t(`components.DatePicker.${key}`);
+  const tr = (key: string) => t(`libs.components.DatePicker.${key}`);
 
   const refDate = new Date();
-  const [val, setVal] = useState<string>(format(refDate, "yyyy-MM-dd"));
+  const ref = useRef<HTMLDivElement>(null);
+  const [val, setVal] = useState<string>(props.value);
 
   const handleDateClick = (d: Date) => {
-    setVal(format(d, "yyyy-MM-dd"));
+    const v = format(d, "yyyy-MM-dd");
+    setVal(v);
+    props.handleChange(v);
   };
 
   const getCal = (date: Date) => {
@@ -36,34 +46,99 @@ export function DatePicker() {
     });
   };
 
+  const numOfMonths = 3;
   const months = [];
-  for (let i = 0; i < 24; i += 1) {
+  for (let i = 0; i < numOfMonths; i += 1) {
     months.push(i);
   }
+  const headDate = startOfMonth(refDate);
+  const lastDate = endOfMonth(addMonths(refDate, numOfMonths - 1));
+
+  useEffect(() => {
+    if (props.autoFocus) {
+      ref.current.focus();
+    }
+  }, []);
 
   return (
     <div
+      ref={ref}
       tabIndex={0}
       className="w-full max-w-[420px] mx-auto relative"
       onKeyDown={(e) => {
         e.preventDefault();
         const key = e.key;
         if (key === "ArrowDown") {
-          setVal(format(addDays(val, 7), "yyyy-MM-dd"));
+          if (val) {
+            const d = addDays(val, 7);
+            if (isAfter(d, lastDate)) {
+              setVal("");
+            } else {
+              setVal(format(d, "yyyy-MM-dd"));
+            }
+          } else {
+            setVal(format(headDate, "yyyy-MM-dd"));
+          }
         } else if (key === "ArrowUp") {
-          setVal(format(addDays(val, -7), "yyyy-MM-dd"));
+          if (val) {
+            const d = addDays(val, -7);
+            if (isBefore(d, headDate)) {
+              setVal("");
+            } else {
+              setVal(format(d, "yyyy-MM-dd"));
+            }
+          } else {
+            setVal(format(lastDate, "yyyy-MM-dd"));
+          }
         } else if (key === "ArrowLeft") {
-          setVal(format(addDays(val, -1), "yyyy-MM-dd"));
+          if (val) {
+            setVal(format(addDays(val, -1), "yyyy-MM-dd"));
+          } else {
+            setVal(format(startOfMonth(refDate), "yyyy-MM-dd"));
+          }
         } else if (key === "ArrowRight") {
-          setVal(format(addDays(val, 1), "yyyy-MM-dd"));
+          if (val) {
+            setVal(format(addDays(val, 1), "yyyy-MM-dd"));
+          } else {
+            setVal(format(startOfMonth(refDate), "yyyy-MM-dd"));
+          }
         } else if (key === "Enter") {
-          console.log("TODO: Dispatch Event(enter? confirm?)");
+          props.handleChange(val);
+        } else if (key === "Escape") {
+          setVal(props.value);
+        } else if (key === "Backspace" || key === "Delete") {
+          setVal("");
         }
       }}
     >
-      <div className="">
+      <div>
         <table className="w-full">
           <thead className="sticky top-0 bg-white">
+            <tr>
+              <th colSpan={7} className="p-2">
+                <div className="flex">
+                  <span className="flex-1 text-left">{val}</span>
+                  <button
+                    className="px-4"
+                    onClick={() => {
+                      setVal("");
+                      props.handleChange("");
+                    }}
+                  >
+                    {tr("Reset")}
+                  </button>
+                  <button
+                    className="pl-4 pr-3"
+                    onClick={() => {
+                      setVal(props.value);
+                      props.handleCancel();
+                    }}
+                  >
+                    {tr("Cancel")}
+                  </button>
+                </div>
+              </th>
+            </tr>
             <tr>
               <th className="p-2 pb-4">{tr("Sunday")}</th>
               <th className="p-2 pb-4">{tr("Monday")}</th>
@@ -74,44 +149,42 @@ export function DatePicker() {
               <th className="p-2 pb-4">{tr("Saturday")}</th>
             </tr>
           </thead>
-          <tbody>
-            {months.map((d) => {
-              const r = addMonths(refDate, d);
-              const cal = getCal(r);
-              return (
-                <>
-                  <tr>
-                    <td className="px-4 pt-8 pb-2 font-bold" colSpan={7}>
-                      <div>{format(r, "yyyy/MM")}</div>
-                    </td>
+          {months.map((d) => {
+            const r = addMonths(refDate, d);
+            const cal = getCal(r);
+            return (
+              <tbody key={d.toString()}>
+                <tr>
+                  <td className="px-4 pt-8 pb-2 font-bold" colSpan={7}>
+                    <div>{format(r, "yyyy/MM")}</div>
+                  </td>
+                </tr>
+                {cal.map((week) => (
+                  <tr key={week.toString()}>
+                    {week.map((day) => (
+                      <td
+                        key={day.toString()}
+                        className="text-center p-2 cursor-pointer"
+                        onClick={() => handleDateClick(day)}
+                      >
+                        {day.getMonth() === r.getMonth() && (
+                          <div
+                            className={clsx(
+                              "flex p-4 rounded-full w-8 h-8 items-center justify-center",
+                              val === format(day, "yyyy-MM-dd") &&
+                                "text-white bg-gray-600"
+                            )}
+                          >
+                            <span>{day.getDate()}</span>
+                          </div>
+                        )}
+                      </td>
+                    ))}
                   </tr>
-                  {cal.map((week) => (
-                    <tr key={week.toString()}>
-                      {week.map((day) => (
-                        <td
-                          key={day.toString()}
-                          className="text-center p-2 cursor-pointer"
-                          onClick={() => handleDateClick(day)}
-                        >
-                          {day.getMonth() === r.getMonth() && (
-                            <div
-                              className={clsx(
-                                "flex p-4 rounded-full w-8 h-8 items-center justify-center",
-                                val === format(day, "yyyy-MM-dd") &&
-                                  "text-white bg-gray-600"
-                              )}
-                            >
-                              <span>{day.getDate()}</span>
-                            </div>
-                          )}
-                        </td>
-                      ))}
-                    </tr>
-                  ))}
-                </>
-              );
-            })}
-          </tbody>
+                ))}
+              </tbody>
+            );
+          })}
         </table>
       </div>
     </div>
