53290858cd18b667ca797dc1d1e7355beeab86e9
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2024-05-19 09:27:49 +0900

    Create libs for api endpoints

---
+++ src/libs/pages/api/index.ts
@@ -0,0 +1,48 @@
+import { createClient } from "@supabase/supabase-js";
+import type { NextApiRequest } from "next";
+import { PrismaClient } from "@prisma/client";
+
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
+
+export const prisma = new PrismaClient();
+
+export function exclude<T, Key extends keyof T>(
+  obj: T,
+  keys: Key[]
+): Omit<T, Key> {
+  return Object.fromEntries(
+    Object.entries(obj).filter(([key]) => !keys.includes(key))
+  ) as Omit<T, Key>;
+}
+
+export async function auth(req: NextApiRequest) {
+  const authorization = req.headers["authorization"];
+
+  if (!authorization) {
+    return {
+      user: null,
+      errorMessage: "Authorization header is required.",
+    };
+  }
+
+  const accessToken = authorization.split(" ")[1];
+  const {
+    data: { user },
+    error: err,
+  } = await supabase.auth.getUser(accessToken);
+
+  if (err) {
+    return {
+      user: null,
+      errorMessage: err.message,
+    };
+  }
+
+  return {
+    user,
+    errorMessage: "",
+  };
+}
