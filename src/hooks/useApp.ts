d2ccdc0224f0ce8b6accaf362f9040ac517a7ca0
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2024-05-21 22:00:57 +0900

    Fix return type in hooks/app

--- src/hooks/useApp.ts
+++ src/hooks/useApp.ts
@@ -3,7 +3,10 @@ import { useGlobalState } from "libs/globalState";
 // useResouce: () => [Resouce, { mutations }, { selectors }]
 // App, Profile, Preferences, TaskList-Task
 
-export const useApp = (): [App, {}] => {
+export const useApp = (): [
+  App,
+  { updateApp: (newApp: Partial<App>) => void },
+] => {
   const [globalState, setGlobalState] = useGlobalState();
 
   const updateApp = (newApp: Partial<App>) => {
