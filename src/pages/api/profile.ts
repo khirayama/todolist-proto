3ae045adf8fd8b9568d8ba34ef3a215d868c4b04
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2024-05-25 15:15:49 +0900

    Implement profile endpoints

---
+++ src/pages/api/profile.ts
@@ -0,0 +1,38 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import type { Profile as ProfileType } from "@prisma/client";
+
+import { prisma, exclude, auth } from "libs/pages/api";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { user, errorMessage } = await auth(req);
+  if (errorMessage) {
+    return res.status(401).json({ error: errorMessage });
+  }
+
+  const unsafeKeys: (keyof ProfileType)[] = ["id", "userId"];
+
+  if (req.method === "GET") {
+    const profile = await prisma.profile.findUnique({
+      where: {
+        userId: user.id,
+      },
+    });
+    return res.json({
+      profile: exclude(profile, unsafeKeys),
+    });
+  }
+  if (req.method === "PATCH") {
+    const profile = await prisma.profile.update({
+      where: {
+        userId: user.id,
+      },
+      data: exclude(req.body, unsafeKeys),
+    });
+    return res.json({
+      profile: exclude(profile, unsafeKeys),
+    });
+  }
+}
