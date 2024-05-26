85563aca1c729d3f8399275ae5c7638d2170c52c
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2024-05-26 09:28:12 +0900

    Use global state snapshot in useApp hooks

--- src/hooks/useApp.ts
+++ src/hooks/useApp.ts
@@ -1,4 +1,7 @@
 import { useGlobalState } from "libs/globalState";
+import { useEffect } from "react";
+
+import { client } from "hooks/common";
 
 // useResouce: () => [Resouce, { mutations }, { selectors }]
 // App, Profile, Preferences, TaskList-Task
@@ -7,16 +10,52 @@ export const useApp = (): [
   App,
   { updateApp: (newApp: Partial<App>) => void },
 ] => {
-  const [globalState, setGlobalState] = useGlobalState();
+  const [globalState, setGlobalState, getGlobalStateSnapshot] =
+    useGlobalState();
+
+  const fetchApp = () => {
+    client()
+      .get("/api/app")
+      .then((res) => {
+        const snapshot = getGlobalStateSnapshot();
+        setGlobalState({
+          ...snapshot,
+          app: {
+            ...snapshot.app,
+            ...res.data.app,
+          },
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    fetchApp();
+  }, []);
 
   const updateApp = (newApp: Partial<App>) => {
+    const snapshot = getGlobalStateSnapshot();
     setGlobalState({
-      ...globalState,
+      ...snapshot,
       app: {
-        ...globalState.app,
+        ...snapshot.app,
         ...newApp,
       },
     });
+    client()
+      .patch("/api/app", newApp)
+      .then((res) => {
+        const snapshot = getGlobalStateSnapshot();
+        setGlobalState({
+          ...snapshot,
+          app: {
+            ...snapshot.app,
+            ...res.data.app,
+          },
+        });
+      });
   };
 
   return [globalState.app, { updateApp }];
