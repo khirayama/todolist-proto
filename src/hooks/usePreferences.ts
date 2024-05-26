c3a5bcaa58d0de7cfdbdc3e1c061a5f0e4cbefbd
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2024-05-26 09:28:30 +0900

    Use global state snapshot in usePreferences hooks

--- src/hooks/usePreferences.ts
+++ src/hooks/usePreferences.ts
@@ -1,7 +1,17 @@
 import { useGlobalState } from "libs/globalState";
+import { useEffect } from "react";
+import { Preferences as PreferencesData } from "@prisma/client";
+
+import { client } from "hooks/common";
 
 // useResouce: () => [Resouce, { mutations }, { selectors }]
 // App, Profile, Preferences, TaskList-Task
+
+const transform = (data: PreferencesData): { preferences: Preferences } => {
+  return {
+    preferences: data,
+  };
+};
 
 export const usePreferences = (): [
   Preferences,
@@ -9,16 +19,52 @@ export const usePreferences = (): [
     updatePreferences: (newPreferences: Partial<Preferences>) => void;
   },
 ] => {
-  const [globalState, setGlobalState] = useGlobalState();
+  const [globalState, setGlobalState, getGlobalStateSnapshot] =
+    useGlobalState();
+
+  const fetchPreferences = () => {
+    client()
+      .get("/api/preferences")
+      .then((res) => {
+        const snapshot = getGlobalStateSnapshot();
+        setGlobalState({
+          ...snapshot,
+          preferences: {
+            ...snapshot.preferences,
+            ...transform(res.data.preferences).preferences,
+          },
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    fetchPreferences();
+  }, []);
 
   const updatePreferences = (newPreferences: Partial<Preferences>) => {
+    const snapshot = getGlobalStateSnapshot();
     setGlobalState({
-      ...globalState,
+      ...snapshot,
       preferences: {
-        ...globalState.preferences,
+        ...snapshot.preferences,
         ...newPreferences,
       },
     });
+    client()
+      .patch("/api/preferences", newPreferences)
+      .then((res) => {
+        const snapshot = getGlobalStateSnapshot();
+        setGlobalState({
+          ...snapshot,
+          preferences: {
+            ...snapshot.preferences,
+            ...transform(res.data.preferences).preferences,
+          },
+        });
+      });
   };
 
   return [
