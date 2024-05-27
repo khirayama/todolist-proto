c419992c3853028e282ef8e0422163a2ac403ed7
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2024-05-27 22:22:48 +0900

    Implement createDebounce

--- src/hooks/common.ts
+++ src/hooks/common.ts
@@ -10,3 +10,13 @@ export const client = () => {
     },
   });
 };
+
+export const createDebounce = () => {
+  let timeoutId = null;
+  return (fn: Function, t: number) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      fn();
+    }, t);
+  };
+};
