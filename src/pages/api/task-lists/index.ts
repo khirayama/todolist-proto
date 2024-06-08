8758ecec4747391581abd296cabcb03b4045c3cc
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2024-06-08 13:34:11 +0900

    Delete needless fields for creating task list endpoint

--- src/pages/api/task-lists/index.ts
+++ src/pages/api/task-lists/index.ts
@@ -14,6 +14,7 @@ export default async function handler(
     }
 
     const newTaskList = req.body;
+    delete newTaskList.shareCode;
     const [taskList, shareCode] = await prisma.$transaction([
       prisma.taskList.create({
         data: newTaskList,
