127fa0cc7e50e8bb727b3eae72fc7a7c537a4fc5
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2024-06-09 15:54:58 +0900

    Set page stack

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 // These styles apply to every route in the application
+import { useEffect } from "react";
 import type { AppProps } from "next/app";
+import qs from "query-string";
 
 import "libs/pages/globals.css";
 
@@ -10,6 +12,15 @@ import { init as initI18n } from "libs/i18n";
 initI18n();
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const query = qs.parse(window.location.search);
+    if (Object.keys(query).length) {
+      const tmp = window.location.href;
+      window.history.replaceState({}, "", "/app");
+      window.history.pushState({}, "", tmp);
+    }
+  }, []);
+
   return (
     <SupabaseProvider>
       <GlobalStateProvider>
