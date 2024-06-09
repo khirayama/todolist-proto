e85d995fcf3b68acec8799a785295448b6a7b965
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2024-06-09 15:54:15 +0900

    Create ParamsSheet component

---
+++ src/libs/components/ParamsSheet.tsx
@@ -0,0 +1,81 @@
+import { useState, useEffect, ReactNode } from "react";
+import * as RadixDialog from "@radix-ui/react-dialog";
+import { clsx } from "clsx";
+import { useRouter } from "next/router";
+import qs from "query-string";
+
+import { Icon } from "libs/components/Icon";
+
+export function ParamsSheet(props: {
+  children: ReactNode;
+  title: string;
+  open: (q?: Query) => boolean;
+}) {
+  const router = useRouter();
+  const query = qs.parse(window.location.search);
+  const [isOpen, setIsOpen] = useState<boolean>(props.open(query));
+
+  useEffect(() => {
+    const handleRouteChange = () => {
+      const query = qs.parse(window.location.search);
+
+      if (!props.open(query)) {
+        setTimeout(() => {
+          setIsOpen(false);
+        }, 600);
+      } else {
+        setIsOpen(true);
+      }
+    };
+
+    // handleRouteChange();
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router]);
+
+  return (
+    <RadixDialog.Root
+      open={isOpen}
+      onOpenChange={(o) => {
+        if (!o) {
+          router.back();
+        }
+      }}
+    >
+      <RadixDialog.Portal>
+        <RadixDialog.Overlay
+          className={clsx(
+            "fixed w-full h-full top-0 left-0 z-[100] bg-gray-900/50",
+            props.open(query) &&
+              `animate-[overlayshow_600ms_cubic-bezier(0.16,1,0.3,1)_forwards]`,
+            !props.open(query) &&
+              `animate-[overlayhide_600ms_cubic-bezier(0.16,1,0.3,1)_forwards]`
+          )}
+        />
+        <RadixDialog.Content
+          className={clsx(
+            "flex flex-col fixed bottom-0 left-1/2 translate-x-[-50%] w-full max-w-2xl mx-auto min-h-[80%] max-h-[95%] rounded-t-lg shadow-lg z-[400] bg-white overflow-hidden",
+            props.open(query) &&
+              `animate-[contentshow_600ms_cubic-bezier(0.16,1,0.3,1)_forwards]`,
+            !props.open(query) &&
+              `animate-[contenthide_600ms_cubic-bezier(0.16,1,0.3,1)_forwards]`
+          )}
+        >
+          <header className="flex w-full p-4 items-center justify-center sticky top-0 bg-white">
+            <div className="flex-1 font-bold text-center">{props.title}</div>
+            <div className="absolute right-0 top-0 p-4">
+              <RadixDialog.Close>
+                <Icon text="close" />
+              </RadixDialog.Close>
+            </div>
+          </header>
+          <div className="flex-1 h-full w-full overflow-scroll">
+            {props.children}
+          </div>
+        </RadixDialog.Content>
+      </RadixDialog.Portal>
+    </RadixDialog.Root>
+  );
+}
