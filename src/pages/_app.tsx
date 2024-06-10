a1705cc373d8d2eae669ff2f538b9e3ac805c41d
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2024-06-10 21:42:58 +0900

    Fix page stack and support fast refresh

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 // These styles apply to every route in the application
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import type { AppProps } from "next/app";
 import qs from "query-string";
 
@@ -12,12 +12,27 @@ import { init as initI18n } from "libs/i18n";
 initI18n();
 
 export default function App({ Component, pageProps }: AppProps) {
+  const isInitialRender = useRef(true);
+  let isFastRefresh = false;
+
   useEffect(() => {
-    const query = qs.parse(window.location.search);
-    if (Object.keys(query).length) {
-      const tmp = window.location.href;
-      window.history.replaceState({}, "", "/app");
-      window.history.pushState({}, "", tmp);
+    isInitialRender.current = false;
+  }, []);
+
+  if (isInitialRender.current) {
+    isFastRefresh = false;
+  } else {
+    isFastRefresh = true;
+  }
+
+  useEffect(() => {
+    if (!isFastRefresh) {
+      const query = qs.parse(window.location.search);
+      if (Object.keys(query).length) {
+        const tmp = window.location.href;
+        window.history.replaceState({}, "", "/app");
+        window.history.pushState({}, "", tmp);
+      }
     }
   }, []);
 
