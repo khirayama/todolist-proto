6b3a2d305842aace4ad5f57762499d5df23fc54e
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2024-06-28 23:34:17 +0900

    Update hooks/common for client

--- src/hooks/common.ts
+++ src/hooks/common.ts
@@ -1,9 +1,10 @@
-import axios from "axios";
+import deepEqual from "fast-deep-equal";
+import axios, { type AxiosRequestConfig, type AxiosResponse } from "axios";
 import { getSession } from "libs/supabase";
 
 export const time = {
   updateDebounce: 600,
-  polling: 5000,
+  polling: 8000,
   pollingLong: 20000,
 };
 
@@ -37,6 +38,7 @@ export const createPolling = () => {
 };
 
 export const client = () => {
+  console.warn("Please migrate to useClient()");
   const session = getSession();
   return axios.create({
     withCredentials: true,
@@ -45,4 +47,47 @@ export const client = () => {
       Authorization: `Bearer ${session?.access_token}`,
     },
   });
+};
+
+const requests: AxiosRequestConfig[] = [];
+
+const newClient = () => {
+  const session = getSession();
+  const r = axios.create({
+    withCredentials: true,
+    headers: {
+      "Cache-Control": "no-cache",
+      Authorization: `Bearer ${session?.access_token}`,
+    },
+  });
+
+  const isRequesting = (options: AxiosRequestConfig) => {
+    return requests.some((op) => deepEqual(op, options));
+  };
+
+  return {
+    sent: function sent<T = any>(
+      options: AxiosRequestConfig
+    ): Promise<AxiosResponse<T>> {
+      if (isRequesting(options)) {
+        console.warn("Same request is already sending. but send again.");
+      }
+      requests.push(options);
+      return new Promise((resolve, reject) => {
+        return r(options)
+          .then(resolve)
+          .catch(reject)
+          .finally(() => {
+            setTimeout(() => {
+              requests.splice(requests.indexOf(options), 1);
+            }, 200);
+          });
+      });
+    },
+    isRequesting,
+  };
+};
+
+export const useClient = () => {
+  return newClient();
 };
