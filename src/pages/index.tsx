e0f6ec02c05a051a16df3c897c91d3a17eb1dd84
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2024-07-05 22:39:29 +0900

    Update index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,13 +5,32 @@ export default function IndexPage() {
   const { isLoggedIn } = useSupabase();
 
   const params = !isLoggedIn ? { drawer: "opened", sheet: "user" } : {};
+
   return (
     <div>
-      <h1>Todo List</h1>
-      <ParamsLink href="/app" params={params}>
-        Log in
-      </ParamsLink>
-      {!isLoggedIn && <ParamsLink href="/app">Use without login</ParamsLink>}
+      <div>
+        <div className="text-center py-16">
+          <h1 className="p-4 text-center">Lightlist</h1>
+          <ParamsLink
+            href="/app"
+            params={params}
+            className="border py-2 px-4 rounded-full"
+          >
+            はじめる
+          </ParamsLink>
+        </div>
+        {!isLoggedIn && (
+          <ParamsLink href="/demo">ログインせずにさわってみる</ParamsLink>
+        )}
+        <div className="p-4 max-w-lg m-auto">
+          <p className="my-4">
+            Lightlistは、とてもシンプルなタスクリストサービスです。
+          </p>
+          <p>
+            タスク管理としてのToDoリストや、買い物リストとしても利用できます。登録していない人ともリストを共有することができます。
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
