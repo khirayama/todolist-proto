d5a419257e45ac7959afd4e8a9120185b741f088
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2024-07-06 21:45:27 +0900

    Update index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ export default function IndexPage() {
 
   return (
     <div>
-      <div>
+      <div className="pb-8">
         <div className="text-center pt-24 pb-4">
           <img
             src="/logo.svg"
@@ -38,6 +38,31 @@ export default function IndexPage() {
           </p>
         </div>
       </div>
+
+      <div className="bg-gray-100 pt-8">
+        <div className="relative aspect-video overflow-hidden max-w-3xl mx-auto">
+          <img
+            className="absolute shadow-2xl bottom-[-100px] w-[80%] min-w-[320px] left-[32px]"
+            src="/screenshot_ja_desktop.png"
+          />
+          <img
+            className="absolute shadow-2xl bottom-[-60px] w-[24%] min-w-[105px] right-[32px] rotate-6"
+            src="/screenshot_ja_mobile.png"
+          />
+        </div>
+      </div>
+
+      <footer className="p-12 text-center">
+        <div className="text-center p-4">
+          <ParamsLink
+            href="/app"
+            params={params}
+            className="border py-2 px-4 rounded-full"
+          >
+            はじめる
+          </ParamsLink>
+        </div>
+      </footer>
     </div>
   );
 }
