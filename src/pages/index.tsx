3ddc8e5ca0ca86d5d2000bc8e6d4b2987524f244
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2024-07-09 22:19:32 +0900

    Update index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,40 @@
+import { useState } from "react";
+
 import { useSupabase } from "libs/supabase";
 import { ParamsLink } from "libs/components/ParamsLink";
+import { useCustomTranslation } from "libs/i18n";
 
 export default function IndexPage() {
   const { isLoggedIn } = useSupabase();
+  const { t, i18n } = useCustomTranslation("pages.index");
 
   const params = !isLoggedIn ? { drawer: "opened", sheet: "user" } : {};
+  const [lang, setLang] = useState(i18n.resolvedLanguage);
+
+  if (lang !== i18n.resolvedLanguage) {
+    i18n.changeLanguage(lang);
+  }
 
   return (
     <div>
+      <header className="max-w-2xl mx-auto text-right py-4">
+        <button
+          className="px-4 py-2 rounded"
+          onClick={() => {
+            setLang("en");
+          }}
+        >
+          English
+        </button>
+        <button
+          className="p-4 py-2 rounded"
+          onClick={() => {
+            setLang("ja");
+          }}
+        >
+          日本語
+        </button>
+      </header>
       <div className="pb-8">
         <div className="text-center pt-24 pb-4">
           <img
@@ -23,18 +50,20 @@ export default function IndexPage() {
             params={params}
             className="border py-2 px-4 rounded-full"
           >
-            はじめる
+            {t("Get started")}
           </ParamsLink>
         </div>
         {!isLoggedIn && (
-          <ParamsLink href="/demo">ログインせずにさわってみる</ParamsLink>
+          <ParamsLink href="/demo">{t("Try without logging in")}</ParamsLink>
         )}
         <div className="p-8 max-w-lg m-auto text-justify">
           <p className="my-4">
-            Lightlistは、とてもシンプルなタスクリストサービスです。
+            {t("Lightlist is a simple task list service.")}
           </p>
           <p>
-            タスク管理としてのToDoリストや、買い物リストとしても利用できます。登録していない人ともリストを共有することができます。
+            {t(
+              "It can be used as a ToDo list for task management or a shopping list. You can share lists with people who are not registered."
+            )}
           </p>
         </div>
       </div>
@@ -43,11 +72,11 @@ export default function IndexPage() {
         <div className="relative aspect-video overflow-hidden max-w-3xl mx-auto">
           <img
             className="absolute shadow-2xl bottom-[-100px] w-[80%] min-w-[320px] left-[32px]"
-            src="/screenshot_ja_desktop.png"
+            src={`/screenshot_${lang}_desktop.png`}
           />
           <img
             className="absolute shadow-2xl bottom-[-60px] w-[24%] min-w-[105px] right-[32px] rotate-6"
-            src="/screenshot_ja_mobile.png"
+            src={`/screenshot_${lang}_mobile.png`}
           />
         </div>
       </div>
@@ -59,7 +88,7 @@ export default function IndexPage() {
             params={params}
             className="border py-2 px-4 rounded-full"
           >
-            はじめる
+            {t("Get started")}
           </ParamsLink>
         </div>
       </footer>
